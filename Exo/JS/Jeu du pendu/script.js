e45e9c9c2f8eb1513187f171f1a128bb7dea0415
Thomas <t.bausiere@gmail.com>
2023-10-25 16:30:36 +0200

    adding 25/10/23 TP for today: Pendu

---
+++ Exo/JS/Jeu du pendu/script.js
@@ -0,0 +1,102 @@
+const words = ["effet", "pendu", "ordinateur", "javascript"];
+let word = words[Math.floor(Math.random() * words.length)];
+let errors = 0;
+let found = new Array(word.length).fill('-').join('');
+const usedLetters = [];
+const drawings = ["", "O", "O-", "O--", "O--<", "O--<|>", "O--<|>-"];
+const letterUsedElem = document.getElementById('letterUsed');
+
+// Générer les boutons pour chaque lettre de l'alphabet
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lettersContainer = document.getElementById('letters');
+letters.split('').forEach(letter => {
+  const button = document.createElement('button');
+  button.textContent = letter;
+  button.disabled = false; 
+  button.addEventListener('click', handleLetterClick);
+  lettersContainer.appendChild(button);
+});
+
+// Initialise l'affichage
+document.getElementById('found').textContent = found;
+document.getElementById('errors').textContent = `Erreurs: ${errors}`;
+
+function handleLetterClick(event) {
+  const clickedButton = event.target;
+  const clickedLetter = clickedButton.textContent.toLowerCase();
+  console.log(`Vous avez cliqué sur ${clickedLetter}`);
+
+  // Ajoute la lettre à la liste des lettres utilisées
+  usedLetters.push(clickedLetter.toUpperCase());
+  updateUsedLetters();
+      // Vérifie si le bouton est déjà désactivé
+  if (clickedButton.disabled) {
+    return;
+  }
+
+  // Désactive le bouton
+  clickedButton.disabled = true;
+  let updatedFound = '';
+  let letterFound = false;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === clickedLetter) {
+      updatedFound += clickedLetter;
+      letterFound = true;
+    } else {
+      updatedFound += found[i];
+    }
+  }
+
+  if (!letterFound) {
+    errors++;
+    document.getElementById('errors').textContent = `Erreurs: ${errors}`;
+    if (errors > 6) {
+      alert("Vous avez perdu!");
+      resetGame();
+    }
+  }
+
+  found = updatedFound;
+  document.getElementById('found').textContent = found;
+
+  if (!found.includes('-')) {
+    alert("Vous avez gagné!");
+    resetGame();
+  }
+}
+
+function updateUsedLetters() {
+    letterUsedElem.innerHTML = '';
+    usedLetters.forEach((letter, index) => {
+      const letterElem = document.createElement('span');
+      letterElem.textContent = letter;
+      letterElem.style.textDecorationLine = 'line-through';
+      letterUsedElem.append(letterElem);
+      
+      // Ajoute une virgule et un espace si ce n'est pas la dernière lettre
+      if (index < usedLetters.length - 1) {
+        const separator = document.createElement('span');
+        separator.textContent = ', ';
+        letterUsedElem.append(separator);
+      }
+    });
+  }
+  
+function resetGame() {
+  errors = 0;
+  word = words[Math.floor(Math.random() * words.length)];
+  found = new Array(word.length).fill('-').join('');
+  usedLetters.length = 0;
+  document.getElementById('found').textContent = found;
+  document.getElementById('errors').textContent = `Erreurs: ${errors}`;
+  updateUsedLetters();
+}
+
+//Utilisation du clavier
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key.toUpperCase();
+  if (letters.includes(keyName)) {
+    document.querySelector(`button:contains('${keyName}')`).click();
+  }
+});
