5ad0e9fc01db8088dd0190b23af6613199d41f3c
Thomas <t.bausiere@gmail.com>
2023-10-30 08:44:55 +0100

    pushing Hangman, jeu du pendu retravaillé todo left: api gestion victoire, regles

--- Exo/JS/Jeu du pendu/script.js
+++ Exo/JS/Jeu du pendu/script.js
@@ -1,116 +1,116 @@
+"use strict";
+
+// 1. Déclarations des variables et constantes
 const words = ["effet", "pendu", "ordinateur", "javascript"];
 let word = words[Math.floor(Math.random() * words.length)];
 let errors = 0;
+let gameOver = false;
 let found = new Array(word.length).fill('-').join('');
-const usedLetters = [];
-const drawings = ["", "O", "O-", "O--", "O--<", "O--<|>", "O--<|>-"];
-const letterUsedElem = document.getElementById('letterUsed');
-
-// Générer les boutons pour chaque lettre de l'alphabet
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lettersContainer = document.getElementById('letters');
-letters.split('').forEach(letter => {
-  const button = document.createElement('button');
-  button.textContent = letter;
-  button.disabled = false; 
-  button.addEventListener('click', handleLetterClick);
-  lettersContainer.appendChild(button);
-});
+const foundElem = document.getElementById('found');
+const drawingDiv = document.getElementById('drawing');
+const resetButton = document.getElementById('resetButton');
+const MAX_ERRORS = 6;
+const  reset = document.querySelector('.reset')
+resetButton.addEventListener('click', resetGame);
 
-// Initialise l'affichage
-document.getElementById('found').textContent = found;
-document.getElementById('errors').textContent = `Erreurs: ${errors}`;
+foundElem.style.fontFamily="Halowen,'Times New Roman', Times, serif";
 
+// 2. Fonctions
 function handleLetterClick(event) {
-  const clickedButton = event.target;
-  const clickedLetter = clickedButton.textContent.toLowerCase();
-  console.log(`Vous avez cliqué sur ${clickedLetter}`);
+    if (gameOver) return;
 
-  // Ajoute la lettre à la liste des lettres utilisées
-  usedLetters.push(clickedLetter.toUpperCase());
-  updateUsedLetters();
-      // Vérifie si le bouton est déjà désactivé
-  if (clickedButton.disabled) {
-    return;
-  }
+    const clickedButton = event.target;
+    const clickedLetter = clickedButton.textContent.toLowerCase();
 
-  // Désactive le bouton
-  clickedButton.disabled = true;
-  let updatedFound = '';
-  let letterFound = false;
+    clickedButton.disabled = true;
 
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === clickedLetter) {
-      updatedFound += clickedLetter;
-      letterFound = true;
-    } else {
-      updatedFound += found[i];
+    let updatedFound = '';
+    let letterFound = false;
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === clickedLetter) {
+            updatedFound += clickedLetter;
+            letterFound = true;
+        } else {
+            updatedFound += found[i];
+        }
     }
-  }
 
-  if (!letterFound) {
-    errors++;
-    document.getElementById('errors').textContent = `Erreurs: ${errors}`;
-    updateImage();
-    if (errors >= 6) {
-      console.log("Vous avez perdu!");
-      
+    if (!letterFound) {
+        errors++;
+        updateImage();
+        if (errors === MAX_ERRORS) {
+            console.log("Vous avez perdu!");
+            gameOver = true;
+            reset.style.display="flex";
+            lettersContainer.style.display="none";
+            return;
+        }
     }
-  }
 
-  found = updatedFound;
-  document.getElementById('found').textContent = found;
+    found = updatedFound;
+    foundElem.textContent = found;
 
-  if (!found.includes('-')) {
-    alert("Vous avez gagné!");
-    resetGame();
-  }
+    if (!found.includes('-')) {
+        alert("Vous avez gagné!");
+        resetGame();
+    }
 }
-
-function updateUsedLetters() {
-    letterUsedElem.innerHTML = '';
-    usedLetters.forEach((letter, index) => {
-      const letterElem = document.createElement('span');
-      letterElem.textContent = letter;
-      letterElem.style.textDecorationLine = 'line-through';
-      letterUsedElem.append(letterElem);
-      
-      // Ajoute une virgule et un espace si ce n'est pas la dernière lettre
-      if (index < usedLetters.length - 1) {
-        const separator = document.createElement('span');
-        separator.textContent = ', ';
-        letterUsedElem.append(separator);
-      }
-    });
-  }
-  
-function resetGame() {
-  errors = 0;
-  word = words[Math.floor(Math.random() * words.length)];
-  found = new Array(word.length).fill('-').join('');
-  usedLetters.length = 0;
-  document.getElementById('found').textContent = found;
-  document.getElementById('errors').textContent = `Erreurs: ${errors}`;
-  updateUsedLetters();
-}
-
-//Utilisation du clavier
-document.addEventListener('keydown', (event) => {
-  const keyName = event.key.toUpperCase();
-  if (letters.includes(keyName)) {
-    document.querySelector(`button:contains('${keyName}')`).click();
-  }
-});
-
 
 function updateImage() {
-    const drawingDiv = document.getElementById('drawing');
-    // Effacer les images précédentes s'il y en a
     drawingDiv.innerHTML = '';
-    if(errors > 0) {
-      const img = document.createElement('img');
-      img.src = `./src/img${errors}.png`;
-      img.alt = `Erreur ${errors}`;
-      drawingDiv.appendChild(img);
+    if (errors > 0) {
+        const img = document.createElement('img');
+        img.src = `./src/img${errors}.png`;
+        img.alt = `Erreur ${errors}`;
+        drawingDiv.appendChild(img);
     }
-  }
+}
+
+function resetGame() {
+    errors = 0;
+    word = words[Math.floor(Math.random() * words.length)];
+    found = new Array(word.length).fill('-').join('');
+    foundElem.textContent = found;
+    Array.from(lettersContainer.children).forEach(btn => btn.disabled = false);
+    setInitialImage();
+    gameOver = false;
+    reset.style.display="none";
+    lettersContainer.style.display="flex";
+
+
+}
+
+function setInitialImage() {
+    drawingDiv.innerHTML = '';
+    const img = document.createElement('img');
+    img.src = './src/background.png';
+    img.alt = 'Image initiale';
+    drawingDiv.appendChild(img);
+}
+
+// 3. Initialisation du jeu et événements
+const letters = 'azertyuiopqsdfghjklmwxcvbn';
+letters.split('').forEach(letter => {
+    const button = document.createElement('button');
+    button.textContent = letter;
+    button.addEventListener('click', handleLetterClick);
+    lettersContainer.appendChild(button);
+});
+
+foundElem.textContent = found;
+
+document.addEventListener('keydown', (event) => {
+    if (gameOver) return;
+
+    const keyName = event.key.toLowerCase();
+    if (letters.includes(keyName)) {
+        const button = Array.from(lettersContainer.children).find(btn => btn.textContent === keyName);
+        if (button && !button.disabled) {
+            button.click();
+        }
+    }
+});
+
+setInitialImage();
