0eff39c3bdb745f1fce3a3eb9a8b08b37f319798
Thomas <t.bausiere@gmail.com>
2023-11-11 14:02:12 +0100

    adding Json for random word

--- Exo/JS/Jeudupendu/script.js
+++ Exo/JS/Jeudupendu/script.js
@@ -1,22 +1,46 @@
 "use strict";
 
-// 1. Déclarations des variables et constantes
-const words = ["effet", "pendu", "ordinateur", "javascript"];
-let word = words[Math.floor(Math.random() * words.length)];
+// Déclarations des variables et constantes
+let words;
+let word = '';
 let errors = 0;
 let gameOver = false;
-let found = new Array(word.length).fill('-').join('');
+let found = '';
 const lettersContainer = document.getElementById('letters');
 const foundElem = document.getElementById('found');
 const drawingDiv = document.getElementById('drawing');
 const resetButton = document.getElementById('resetButton');
 const MAX_ERRORS = 6;
-const  reset = document.querySelector('.reset')
+const reset = document.querySelector('.reset');
 resetButton.addEventListener('click', resetGame);
 
-foundElem.style.fontFamily="Halowen,'Times New Roman', Times, serif";
+// Chargement du fichier JSON avec fetch
+async function loadWords() {
+    const response = await fetch('theme.json'); // Assurez-vous que ce chemin est correct
+    const data = await response.json();
+    words = data;
+}
 
-// 2. Fonctions
+// Fonction pour obtenir un mot aléatoire
+function getRandom(a) {
+    let alph = a[Math.floor(Math.random() * a.length)],
+        wordsList = alph.Listes[Math.floor(Math.random() * alph.Listes.length)],
+        mot = wordsList.Mots[Math.floor(Math.random() * wordsList.Mots.length)];
+    console.log(alph, wordsList, mot);
+    if (mot) {
+        return mot.toLowerCase();
+    } else {
+        return getRandom(a);
+    }
+}
+
+// Fonction pour mettre à jour le mot
+function updateWord() {
+    found = new Array(word.length).fill('-').join('');
+    foundElem.textContent = found;
+}
+
+// Fonction pour gérer le clic sur une lettre
 function handleLetterClick(event) {
     if (gameOver) return;
 
@@ -43,8 +67,8 @@ function handleLetterClick(event) {
         if (errors === MAX_ERRORS) {
             console.log("Vous avez perdu!");
             gameOver = true;
-            reset.style.display="flex";
-            lettersContainer.style.display="none";
+            reset.style.display = "flex";
+            lettersContainer.style.display = "none";
             return;
         }
     }
@@ -58,59 +82,67 @@ function handleLetterClick(event) {
     }
 }
 
+// Fonction pour mettre à jour l'image
 function updateImage() {
     drawingDiv.innerHTML = '';
     if (errors > 0) {
         const img = document.createElement('img');
-        img.src = `./src/img${errors}.png`;
+        img.src = `./src/img${errors}.png`; // Assurez-vous que ces chemins sont corrects
         img.alt = `Erreur ${errors}`;
         drawingDiv.appendChild(img);
     }
 }
 
+// Fonction pour réinitialiser le jeu
 function resetGame() {
     errors = 0;
-    word = words[Math.floor(Math.random() * words.length)];
-    found = new Array(word.length).fill('-').join('');
-    foundElem.textContent = found;
+    word = getRandom(words);
+    updateWord();
     Array.from(lettersContainer.children).forEach(btn => btn.disabled = false);
     setInitialImage();
     gameOver = false;
-    reset.style.display="none";
-    lettersContainer.style.display="flex";
-
-
+    reset.style.display = "none";
+    lettersContainer.style.display = "flex";
 }
 
+// Fonction pour définir l'image initiale
 function setInitialImage() {
     drawingDiv.innerHTML = '';
     const img = document.createElement('img');
-    img.src = './src/background.png';
+    img.src = './src/background.png'; // Assurez-vous que ce chemin est correct
     img.alt = 'Image initiale';
     drawingDiv.appendChild(img);
 }
 
-// 3. Initialisation du jeu et événements
-const letters = 'azertyuiopqsdfghjklmwxcvbn';
-letters.split('').forEach(letter => {
-    const button = document.createElement('button');
-    button.textContent = letter;
-    button.addEventListener('click', handleLetterClick);
-    lettersContainer.appendChild(button);
-});
+// Initialisation du jeu
+async function initGame() {
+    await loadWords();
+    word = getRandom(words);
+    updateWord();
 
-foundElem.textContent = found;
+    // Création des boutons de lettres
+    const letters = 'azertyuiopqsdfghjklmwxcvbn';
+    letters.split('').forEach(letter => {
+        const button = document.createElement('button');
+        button.textContent = letter;
+        button.addEventListener('click', handleLetterClick);
+        lettersContainer.appendChild(button);
+    });
 
-document.addEventListener('keydown', (event) => {
-    if (gameOver) return;
+    // Gestion des événements clavier
+    document.addEventListener('keydown', (event) => {
+        if (gameOver) return;
 
-    const keyName = event.key.toLowerCase();
-    if (letters.includes(keyName)) {
-        const button = Array.from(lettersContainer.children).find(btn => btn.textContent === keyName);
-        if (button && !button.disabled) {
-            button.click();
+        const keyName = event.key.toLowerCase();
+        if (letters.includes(keyName)) {
+            const button = Array.from(lettersContainer.children).find(btn => btn.textContent === keyName);
+            if (button && !button.disabled) {
+                button.click();
+            }
         }
-    }
-});
+    });
 
-setInitialImage();
+    setInitialImage();
+}
+
+initGame();
